99a94aef297b1c92cd75a54730f81f1d696ef762
jermane <jermainjoseph@Jermains-iMac.local>
2016-10-26 00:41:09 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,32 @@
+// ES6 Component
+
+//Import React and ReactDOM
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import Search Component
+import Search from './components/search.component';
+
+// Import Details Component
+import Details from './components/details.component';
+
+// Component Class
+class App extends React.Component {
+    // render method is most important
+    // render method returns JSX template
+    render() {
+        return (
+            <div>
+                <Search />
+                <Details title= {'Track title'} />
+            </div>
+        );
+    }
+}
+
+// Render to ID content in the DOM
+ReactDOM.render(
+    <App/ >,
+    document.getElementById('content')
+);
